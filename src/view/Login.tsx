f22ac979a5d2211d68301fb2ad948d52163f489a
K-ongpop Nualsaard <kongpopku@ku.th>
2023-12-14 20:16:21 +0700

    updete-loginView

--- src/view/Login.tsx
+++ src/view/Login.tsx
@@ -2,23 +2,24 @@ import { Link } from 'react-router-dom'
 import { useLineContext } from '../context/LineContext'
 
 const Login = () => {
-    const {mainLine ,dataLine} = useLineContext()
+    const {mainLine } = useLineContext()
   return (
     <div className='fixed w-full z-[20]'>
-      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero min-h-screen bg-center bg-no-repeat" style={{backgroundImage: 'url(/checkout/bg-login.png)'}}>
+ 
   <div className="hero-content text-center text-neutral-content">
-    <div className=" w-[350px] sm:w-[600px] lg:w-[700px] h-[80vh] bg-[#8e6646] flex justify-center items-center rounded-[30px] ">
+    <div className=" w-[350px] sm:w-[600px] lg:w-[700px] h-[75vh] mt-[-40px] bg-[#e7e6ee79]  flex justify-center items-center rounded-[30px] ">
       <div className=' w-full justify-center items-center  '>
-    <h1 className= ' text-[50px] md:text-[72px] font-[600]'>Welcome To </h1>
-     <img  className=" m-[auto] md:w-[250px]"src="/checkout/logoblack.png" alt="" />
-     
+    <h1 className= ' text-[50px] md:text-[72px] lg:text-[100px] font-[600] text-[#8e6646]'>Welcome to </h1>
+     <img  className=" m-[auto] md:w-[250px]"src="/logo.png" alt="" />
+      <div className='p-[20px] bg-yellow-50 m-[auto] md:w-[250px] mt-[10px]'>
       <img className='m-[auto] md:w-[250px] mt-[10px]' src="/checkout/qrcode.png" alt="" />
-      <p className=' w-[200px] md:w-[250px] m-[auto] bg-green-500 font-[400] md:text-[20px] '>scan QR code by App Line</p>
+      </div>
+      <p className=' w-[200px] md:w-[250px] m-[auto]  font-[400] md:text-[20px] '>scan QR code by App Line</p>
       <div className='mt-[20px] w-[200px] md:w-[250px] m-[auto]'>
         
-      <button onClick={mainLine} className="btn btn-green  w-full">{dataLine ? 'เข้าสู่ระบบ' : 'LoginLine' }</button>
-      <Link to={'/Home'}><button className="btn btn-primary w-full mt-[10px] ">Get Started</button></Link>
+      <button onClick={mainLine} className="btn bg-[#00b900]  text-white text-[20px] w-full">Login Line</button>
+      <Link to={'/Home'}><button className="btn bg-[#8e6646] text-white text-[20px] w-full mt-[10px] ">เยี่ยมชมเว็บไซต์</button></Link>
       </div>
       </div>
       

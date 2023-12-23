53703dacc7750678469d20764fa0189c84a85105
K-ongpop Nualsaard <kongpopku@ku.th>
2023-12-23 15:35:50 +0700

    delete-useeffect

--- src/component/nav/Listmenu.tsx
+++ src/component/nav/Listmenu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import InputSearch from "../search/InputSearch ";
 import Close from "./Close";

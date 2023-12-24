b10130b8fa58af135b1ca2bec69194b6d16cb49a
K-ongpop Nualsaard <kongpopku@ku.th>
2023-12-24 12:06:06 +0700

    pushnotfound

--- src/view/NotFound.tsx
+++ src/view/NotFound.tsx
@@ -2,7 +2,12 @@
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="w-[100%] h-[100vh] fixed bg-white z-20 top-0 flex justify-center items-center bg-[url('/checkout/BG-404.png')]" >
+       <div className=" max-w-7xl w-full h-[90%] flex flex-col justify-center items-center ">
+          <img  className="w-[300px] h-[150px]" src="/checkout/logoblack.png" alt="" />
+          <h1 className=" text-[100px] font-[600] text-[#8e6646]">404 NOT FOUND</h1>
+       </div>
+    </div>
   )
 }
 
